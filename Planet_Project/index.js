c1b0d7116292d7ee09447eb05e2431663f6c8597
Faraz <farazatgithub@gmail.com>
2023-01-16 11:14:34 +0500

    Planet-Project and read stream of data

---
+++ Planet_Project/index.js
@@ -0,0 +1,14 @@
+const {parse} = require("csv-parse")
+const fs = require("fs")
+
+const results = []
+
+fs.createReadStream("kepler_data.csv")
+  .on("data", (data)=> {
+      results.push(data)
+      console.log(results)
+  }).on("error", (err) => {
+     console.log(err)
+  }).on("end", () => {
+       console.log("done!")
+  })
