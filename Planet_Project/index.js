d2cdb4ba6b3bb90ac715330e5eb66be4b59e44d3
Faraz <farazatgithub@gmail.com>
2023-01-17 13:01:33 +0500

    filtered the habitable planets

--- Planet_Project/index.js
+++ Planet_Project/index.js
@@ -1,18 +1,37 @@
-const {parse} = require("csv-parse")
-const fs = require("fs")
+const { parse } = require("csv-parse");
+const fs = require("fs");
 
-const results = []
+const results = [];
+
+const habitablePlanets = [];
+
+// features for the habitable planets 👇
+const isHabitable = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
 
 fs.createReadStream("kepler_data.csv")
-  .pipe(parse({
-    comment:"#",
-    columns:true
-  }))
-  .on("data", (data)=> {
-      results.push(data)
-      console.log(results)
-  }).on("error", (err) => {
-     console.log(err)
-  }).on("end", () => {
-       console.log("done!")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    // pushing only the habitable planets
+    if (isHabitable(data)) {
+      habitablePlanets.push(data);
+    }
   })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(`${habitablePlanets.length} habitable planets found `);
+    console.log("done!");
+  });
