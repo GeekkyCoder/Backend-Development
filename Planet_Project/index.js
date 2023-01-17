c1b9de84aec90c5fa888a9a9c7a551b5936c4de1
Faraz <farazatgithub@gmail.com>
2023-01-17 13:13:07 +0500

    names of habitable planets

--- Planet_Project/index.js
+++ Planet_Project/index.js
@@ -32,6 +32,20 @@ fs.createReadStream("kepler_data.csv")
     console.log(err);
   })
   .on("end", () => {
+    console.log(habitablePlanets.map(planet => planet["kepler_name"]))
     console.log(`${habitablePlanets.length} habitable planets found `);
-    console.log("done!");
   });
+
+  // these 8 planets are habitable 
+  // [
+  //   'Kepler-1652 b',
+  //   'Kepler-1410 b',
+  //   'Kepler-296 A f',
+  //   'Kepler-442 b',
+  //   'Kepler-296 A e',
+  //   'Kepler-1649 b',
+  //   'Kepler-62 f',
+  //   'Kepler-452 b'
+  // ]
+
+  // 8 habitable planets found
