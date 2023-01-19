797b6249765d5d9c3420db536f6841c5ff5b1549
Faraz <farazatgithub@gmail.com>
2023-01-19 16:38:57 +0500

    404 page

--- Web_Servers/http_routing.js
+++ Web_Servers/http_routing.js
@@ -1,13 +1,27 @@
 const { createServer } = require("http");
 
-const requestListener = (req, res) => {
-  const { url } = req;
-  if (url === "/") res.write("<h1>home page</h1>");
-  if (url === "/messages") res.write("<h1>messeages page</h1>");
-  if (url === "/friends") res.write("<h1>friends page</h1>");
-  if (url === "/friends/friend") res.write("<h1>Faraz is my friend</h1>");
-  if (url === "/items") res.write("<h1>items page</h1>");
-};
+// const requestListener = (req, res) => {
+//   const { url } = req;
+//   if (url === "/") res.write("<h1>home page</h1>");
+//   if (url === "/messages") res.write("<h1>messeages page</h1>");
+//   if (url === "/friends") res.write("<h1>friends page</h1>");
+//   if (url === "/friends/friend") res.write("<h1>Faraz is my friend</h1>");
+//   if (url === "/items") res.write("<h1>items page</h1>");
+// };
+
+
+//reqest listener for bad request 404
+const requestListener = (req, res) => { 
+    const { url } = req;
+    if (url === "/") { 
+        res.write("<h1>home page</h1>") 
+        res.end()
+    } 
+    else { 
+        res.statusCode = 404
+        res.end()
+    }
+}
 
 const server = createServer(requestListener);
 
