eaca85872fcfbef61294ad70f02ffa07b1cfe872
Faraz <farazatgithub@gmail.com>
2023-02-08 09:04:14 +0500

    added handlebars(hsb) templating engine package for serving dynamic data from server

--- Express/express.js
+++ Express/express.js
@@ -10,7 +10,18 @@ const app = express();
 const PORT = 3000;
 
 
+app.set("view engine","hbs")
+app.set("views",path.join(__dirname,"views"))
+
+app.get("/", (req,res) => {
+  res.render("index", {
+    title:"My friends are very clever",
+    caption:"Pakistan is beatutiful"
+  })
+})
+
 app.use("/site", express.static(path.join(__dirname,"public")))
+
 
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
